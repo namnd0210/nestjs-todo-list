0360f60a07eb1c7d86895650d61daecd1bd9a3d2
Duc nam <namnd0210@gmail.com>
2022-07-18 17:13:09 +0700

    fix login auth local

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+
+export type User = any;
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  private readonly users = [
+    {
+      id: 1,
+      name: 'user1',
+      username: 'user1',
+      password: '1',
+    },
+    {
+      id: 2,
+      name: 'user2',
+      username: 'user2',
+      password: '2',
+    },
+  ];
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
   }
 }
