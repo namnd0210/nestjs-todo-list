966e25ed1f77f8bb8bad1274e048c414d22956ab
Duc nam <namnd0210@gmail.com>
2022-07-19 09:13:18 +0700

    implement protected route and JQT strategy guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { LocalStrategy } from './local.strategy';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   imports: [
     UsersModule,
     PassportModule,
